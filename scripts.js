8023e89adc71641d3eec70912827891b16d21704
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-01-13 11:40:46 -0500

    checks time every second. chooses contrasting color for text as either black or white. changes h1 in html every second to reflect hexcode

--- scripts.js
+++ scripts.js
@@ -2,16 +2,39 @@
 var sheet = document.styleSheets[0];
 // console.log(sheet)
 
-//get time
-var d = new Date();
-var hours = d.getHours();
-var minutes = d.getMinutes();
-var seconds = d.getSeconds();
 
-//create hex code
-var hexString = "#"+hours+minutes+seconds;
-console.log(hexString);
-var styleRule = "body { background-color: " + hexString + "; }"
-console.log(styleRule);
+//jQuery i found that runs function each second
+window.setInterval(function(){
+	//get time
+	var d = new Date();
+	var hours = d.getHours();
+	var minutes = d.getMinutes();
+	var seconds = d.getSeconds();
 
-sheet.insertRule(styleRule, 1);
+	//create hex code
+	var hexString = "#"+hours+minutes+seconds;
+	console.log(hexString);
+
+	//should font be black or white?
+	var color = "";
+	function getContrast(hexcolor){
+    	var hexNum = parseInt(hexcolor, 16)
+    	if (hexNum > 0xffffff/2){
+    		color = "black";
+    	} else {
+    		color = "white";
+    	}
+	}
+	getContrast(hexString);
+	console.log(color);
+
+	//make style rule
+	var styleRule = "body { background-color: " + hexString + "; color: " + color +";}"
+	console.log(styleRule);
+
+	sheet.insertRule(styleRule, 0);
+
+	//change page text
+	document.getElementById("clock").innerText = hexString;
+}, 1000);
+
